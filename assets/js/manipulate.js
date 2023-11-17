c9c415e1b92fd347db554690ed63c2b9575f6539
harmony <harmony.rosenberg94@gmail.com>
2023-11-16 19:54:54 -0600

    phase 3

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,22 @@
-function changeTitle() {
+export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
 
     // Your code here
+    document.title = "Your name's Portfolio"
 }
 
-function changeHeader() {
+export function changeHeader() {
     // Change the name in the h1 of the page to your name
 
-    // Your code here
+    document.h1 = "my page"
 }
 
-function changeAboutMe() {
+export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
 
     // Your code here
+
+    const aboutMe = document.querySelector('.section p')
 }
+
